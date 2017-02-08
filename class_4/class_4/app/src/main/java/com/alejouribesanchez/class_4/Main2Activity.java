b99bdbff6d1f9e77183552c19f5abeda944e9661
Alejo <alejouribesanchez@gmail.com>
2017-02-08 16:55:12 -0500

    class_4

--- class_4/class_4/app/src/main/java/com/alejouribesanchez/class_4/Main2Activity.java
+++ class_4/class_4/app/src/main/java/com/alejouribesanchez/class_4/Main2Activity.java
@@ -1,7 +1,10 @@
 package com.alejouribesanchez.class_4;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        LinearLayout linearLayout = (LinearLayout)findViewById(R.id.activity_main2);
+        linearLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Main2Activity.this, Main3Activity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
