500547c028ae80a6a1bcb0810d08ea4221efa77a
Alejo <alejouribesanchez@gmail.com>
2017-02-21 19:04:03 -0500

    class_8

--- class_8/class_8/app/src/main/java/com/alejouribesanchez/class_8/MainActivity.java
+++ class_8/class_8/app/src/main/java/com/alejouribesanchez/class_8/MainActivity.java
@@ -1,13 +1,45 @@
 package com.alejouribesanchez.class_8;
 
+
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  implements BlankFragment.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Paso 1: Obtener la instancia del administrador de fragmentos
+                FragmentManager fragmentManager = getSupportFragmentManager();
+
+                //Paso 2: Crear una nueva transacción
+                FragmentTransaction transaction = fragmentManager.beginTransaction();
+
+                //Paso 3: Crear un nuevo fragmento y añadirlo
+                BlankFragment fragment = new BlankFragment();
+                transaction.add(R.id.contenedor, fragment);
+
+                //Paso 4: Confirmar el cambio
+                transaction.commit();
+
+            }
+        });
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
